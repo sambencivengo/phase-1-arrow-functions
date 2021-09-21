76e669e84d3ef452faba9816814c7ff45eb5da7c
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-21 09:29:10 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide (num = 2000){
+    let result;
+    result = num / 100;
+    return result;
+}
+
+const square = num => num * num;
+
+const add = (int1, int2) => int1 + int2;
+console.log(add(6,80));
